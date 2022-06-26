d64fedd953a1ad974362a7c8cfcf3423d584c399
md3584 <103767620+md3584@users.noreply.github.com>
2022-06-26 16:30:32 +0530

    Add files via upload

---
+++ filehandling.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main()
+{
+        FILE *fs1, *fs2, *ft;
+        char ch, file1[50], file2[50], file3[50];
+        printf("Enter name of first file :: ");
+        gets(file1);
+        printf("\nEnter name of second file :: ");
+        gets(file2);
+        printf("\nEnter name of file which will store contents of two files :: ");
+        gets(file3);
+        fs1 = fopen(file1,"r");
+        fs2 = fopen(file2,"r");
+        if( fs1 == NULL || fs2 == NULL ) {
+                perror("Error ");
+                printf("Press any key to exit...\n");
+                exit(EXIT_FAILURE);
+        }
+        ft = fopen(file3,"w");
+        if( ft == NULL ) {
+                perror("Error ");
+                printf("Press any key to exit...\n");
+                exit(EXIT_FAILURE);
+        }
+        while( ( ch = fgetc(fs1) ) != EOF )
+              fputc(ch,ft);
+        while( ( ch = fgetc(fs2) ) != EOF )
+              fputc(ch,ft);
+        printf("\nTwo files were merged into %s file successfully.\n",file3);
+        fclose(fs1);
+        fclose(fs2);
+        fclose(ft);
+        return 0;
+}
